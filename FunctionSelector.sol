77b7dda3773f61ae9fabc2b471975a9032ea7f6a
Pedro Leite <pedroaug.fl@gmail.com>
2022-02-11 06:09:10 -0300

    Update FunctionSelector.sol

--- FunctionSelector.sol
+++ FunctionSelector.sol
@@ -2,13 +2,6 @@
 
 pragma solidity ^0.8.10;
 
-contract FunctionSelector {
-    function getSelector(string calldata _func) external pure returns (bytes4) {
-        return bytes4(keccak256(bytes(_func)));
-        // if you input "transfer(address,uint256)" on this function
-        // you will get 0xa9059cbb, which is the Function Selector
-    }
-}
 
 contract Receiver {
     event Log(bytes data);
@@ -19,5 +12,14 @@ contract Receiver {
         // Function selector: 0xa9059cbb
         // address to: 0000000000000000000000005b38da6a701c568545dcfcb03fcb875f56beddc4
         // uint amount: 000000000000000000000000000000000000000000000000000000000000000b
+    }
+}
+
+
+contract FunctionSelector {
+    function getSelector(string calldata _func) external pure returns (bytes4) {
+        return bytes4(keccak256(bytes(_func)));
+        // if you input "transfer(address,uint256)" on this function
+        // you will get 0xa9059cbb, which is the Function Selector
     }
 }
