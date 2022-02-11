af82f16305f56d522775b4d29123814d43a143a6
Pedro Leite <pedroaug.fl@gmail.com>
2022-02-11 06:08:32 -0300

    Create FunctionSelector.sol

---
+++ FunctionSelector.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.10;
+
+contract FunctionSelector {
+    function getSelector(string calldata _func) external pure returns (bytes4) {
+        return bytes4(keccak256(bytes(_func)));
+        // if you input "transfer(address,uint256)" on this function
+        // you will get 0xa9059cbb, which is the Function Selector
+    }
+}
+
+contract Receiver {
+    event Log(bytes data);
+
+    function transfer(address _to, uint _amount) external {
+        emit Log(msg.data);
+        // data is
+        // Function selector: 0xa9059cbb
+        // address to: 0000000000000000000000005b38da6a701c568545dcfcb03fcb875f56beddc4
+        // uint amount: 000000000000000000000000000000000000000000000000000000000000000b
+    }
+}
