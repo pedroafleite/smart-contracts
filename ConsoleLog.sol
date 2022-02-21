b495cd9ae8aaa63a61b7a7e32f58d7c4ba79007e
Pedro Leite <pedroaug.fl@gmail.com>
2022-02-21 19:11:04 -0300

    Create ConsoleLog.sol

---
+++ ConsoleLog.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8;
+
+import "hardhat/console.sol";
+
+
+contract HardhatContract {
+    function test(uint amount) external {
+        uint x = 123;
+        console.log(msg.sender, amount, x);
+        console.log(true, "thinking it through");
+    }
+}
+
+
+// Old way of console log
+contract MyContract {
+    event Log(address sender, uint amount, uint x);
+
+    function test(uint amount) external {
+        uint x = 123;
+        //code
+        emit Log(msg.sender, amount, x);
+    }
+}
